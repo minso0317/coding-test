b8bb2f4a0af3cca36ce91e4c4c22ec8582743c0e
minso0317 <minso0317@naver.com>
2025-08-05 18:19:49 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,12 +1,14 @@
-def solution(angle):    
-    if 0 < angle < 90:
-        return 1
-    elif angle == 90:
-        return 2
-    elif 90 < angle < 180:
-        return 3
-    elif angle == 180:
-        return 4
-    else:
-        return None
+def solution(angle):
+    answer = 0
     
+    if 0 < angle < 90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif 90 < angle < 180:
+        answer = 3
+    elif angle == 180:
+        answer = 4
+    else:
+        answer = None
+    return answer
