9ff7239324ad679be470455e9bbd9809e61ce816
minso0317 <minso0317@naver.com>
2025-08-05 18:34:28 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -2,7 +2,8 @@ def solution(n, k):
     answer = 0
     lamb = 12000
     drink = 2000
+    service_drink = n // 10
     
-    answer = lamb*n + drink*(k - (n // 10))
+    answer = lamb*n + drink*(k - service_drink)
     
     return answer
